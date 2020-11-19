7f795996efe0e19a794ec32e5007f34d12afeb8b
chung200121 <chungnqph12098@fpt.edu.vn>
2020-11-20 00:19:46 +0700

    Them Trang Ban Va Menu

--- DuAn1/src/duan1/TrangChu.java
+++ DuAn1/src/duan1/TrangChu.java
@@ -5,6 +5,12 @@
  */
 package duan1;
 
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.border.AbstractBorder;
+import javax.swing.border.EmptyBorder;
+
+
 /**
  *
  * @author Chung
@@ -16,6 +22,11 @@ public class TrangChu extends javax.swing.JFrame {
      */
     public TrangChu() {
         initComponents();
+//        ImageIcon icon1=new ImageIcon("src//image//nen.jpg");
+//        Image icon=icon1.getImage();
+//                ImageIcon icon2 = new ImageIcon(icon.getScaledInstance(lblImage.getWidth(), lblImage.getHeight(), Image.SCALE_SMOOTH));
+//                lblImage.setIcon(icon2);btnKey.setBorder(new EmptyBorder(20, 20, 20, 20));
+
     }
 
     /**
@@ -27,39 +38,70 @@ public class TrangChu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        btnNhanVien = new javax.swing.JButton();
+        btnMenu = new javax.swing.JButton();
+        btnBan = new javax.swing.JButton();
+        btnDangXuat = new javax.swing.JButton();
+        btnDanhGia = new javax.swing.JButton();
+        btnDMK = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(1042, 683));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/chìa.png"))); // NOI18N
-        jButton1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jButton1.setBorderPainted(false);
-        jButton1.setContentAreaFilled(false);
-        jButton1.setOpaque(false);
-        jButton1.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/image/chìa2.png"))); // NOI18N
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 450, 170, 80));
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(204, 204, 0));
+        jLabel4.setText("Welcome, ");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 260, -1, -1));
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/logo.png"))); // NOI18N
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, 250, 210));
+        jLabel3.setBackground(new java.awt.Color(255, 255, 51));
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 15)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(204, 204, 0));
+        jLabel3.setText("Version: 1.0.0");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 230, -1, -1));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/nen.jpg"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        btnNhanVien.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/nhanvien.png"))); // NOI18N
+        btnNhanVien.setBorderPainted(false);
+        btnNhanVien.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/image/nhanvien1.png"))); // NOI18N
+        getContentPane().add(btnNhanVien, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 280, 180, 90));
+
+        btnMenu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/menu.png"))); // NOI18N
+        btnMenu.setBorderPainted(false);
+        btnMenu.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/image/menu1.png"))); // NOI18N
+        getContentPane().add(btnMenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 140, 180, 90));
+
+        btnBan.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/ban.png"))); // NOI18N
+        btnBan.setBorderPainted(false);
+        btnBan.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/image/ban1.png"))); // NOI18N
+        getContentPane().add(btnBan, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 280, 180, 90));
+
+        btnDangXuat.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/dangxuat.png"))); // NOI18N
+        btnDangXuat.setBorderPainted(false);
+        btnDangXuat.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/image/dangxuat1.png"))); // NOI18N
+        getContentPane().add(btnDangXuat, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 420, 180, 90));
+
+        btnDanhGia.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/danhgia.png"))); // NOI18N
+        btnDanhGia.setBorderPainted(false);
+        btnDanhGia.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/image/danhgia1.png"))); // NOI18N
+        getContentPane().add(btnDanhGia, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 420, 180, 90));
+
+        btnDMK.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/chìa.png"))); // NOI18N
+        btnDMK.setBorderPainted(false);
+        btnDMK.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/image/chìa2.png"))); // NOI18N
+        getContentPane().add(btnDMK, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 420, 180, 90));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/logo.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, -1, -1));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/nen.jpg"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -97,8 +139,15 @@ public class TrangChu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnBan;
+    private javax.swing.JButton btnDMK;
+    private javax.swing.JButton btnDangXuat;
+    private javax.swing.JButton btnDanhGia;
+    private javax.swing.JButton btnMenu;
+    private javax.swing.JButton btnNhanVien;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     // End of variables declaration//GEN-END:variables
 }
